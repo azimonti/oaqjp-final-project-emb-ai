2a783cc59bb1e471db3b7609f2e921af96511873
Marco Azimonti <git@example.com>
2024-10-07 07:22:22 -0400

    added server

---
+++ server.py
@@ -0,0 +1,37 @@
+'''
+/*****************/
+/*   server.py   */
+/*  Version 1.0  */
+/*   2024/10/06  */
+/*****************/
+'''
+from flask import Flask, render_template, request
+import sys
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/emotionDetector')
+def emotionDetector():
+    input_text = request.args.get('textToAnalyze')
+    output = emotion_detector(input_text)
+    # error or exception check
+    if isinstance(output, str):
+        return output
+
+    result="For the given statement, the system response is " \
+        f"'anger': {output['anger']}, 'disgust': {output['disgust']}, " \
+        f"'fear': {output['fear']}, 'joy': {output['joy']} and "\
+        f"'sadness': {output['sadness']}. "\
+        f"The dominant emotion is {output['dominant_emotion']}."
+    return result
+
+
+if __name__ == '__main__':
+    if sys.version_info[0] < 3:
+        raise 'Must be using Python 3'
+app.run(host='localhost', port=5000)
